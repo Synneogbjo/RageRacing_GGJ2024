d7ba3b2b45243ad53f1f3d9c455fdad29cb963b6
Synneogbjo <91120023+Synneogbjo@users.noreply.github.com>
2024-01-28 14:09:25 +0100

    Added map

---
+++ Source/RageRacing_GGJ2024/RageRacing_GGJ2024.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "RageRacing_GGJ2024.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RageRacing_GGJ2024, "RageRacing_GGJ2024" );
